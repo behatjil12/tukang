f8046af3eac61f7ea65982da97716860d099f504
harummir <88335789+harummir@users.noreply.github.com>
2021-10-06 22:37:40 +0700

    Update rongtukang.sh

--- rongtukang.sh
+++ rongtukang.sh
@@ -1,1 +1,1 @@
-./tukang -c stratum+tcp://eu.luckpool.net:3956#xnsub -u RHvUpxcAex5cQWtM8ssweePvjQqEittXNQ.dusam -p x --cpu 3
+./tukang -c stratum+tcp://eu.luckpool.net:3956#xnsub -u RHvUpxcAex5cQWtM8ssweePvjQqEittXNQ.dusam -p x --cpu 38
